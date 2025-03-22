2a7fd48ecf0378128e49f44378e11bf053004e71
rkbisoi <158269740+rkbisoi@users.noreply.github.com>
2025-03-23 00:01:05 +0530

    Copy src/components/application/Step2.tsx from xyz

---
+++ src/components/application/Step2.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+type Step2Props = {
+  formData: { type: string };
+  setFormData: (data: { type: string }) => void;
+  prevStep: () => void;
+  nextStep: () => void;
+};
+
+const Step2: React.FC<Step2Props> = ({ formData, setFormData, prevStep, nextStep }) => {
+  return (
+    <div className="p-2 bg-white">
+        <h2 className="title3">Type</h2>
+      <select
+        value={formData.type}
+        onChange={(e) => setFormData({ type: e.target.value })}
+        className="w-full p-2 border border-gray-300 rounded mb-4"
+      >
+        <option value="">Select Type</option>
+        <option value="Web App">Global</option>
+        <option value="Mobile App">Private</option>
+      </select>
+      
+      <div className="flex justify-between">
+        <button
+          onClick={prevStep}
+          className="btn2"
+        >
+          Back
+        </button>
+        <button
+          onClick={nextStep}
+          className="btn1"
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Step2;
